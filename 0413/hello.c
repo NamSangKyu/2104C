0ae9fbac4f2b248a48f3afb9f75c2a0769110ecd
nam2626 <nam2626@gmail.com>
2021-04-13 21:13:42 +0900

    출력 예쩨

--- 0413/hello.c
+++ 0413/hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("Hello World\n");
+	printf("Hello World1\n"); //문자열은 "" 묶어줘야 데이터로 인식
+	printf("Hello World2\n");
+	printf("Hello World3\n");
 	return 0;
 }
