963521c12c3756a98aa9c2b742fef5834368c9fa
NamSangKyu <nam2626@gmail.com>
2021-05-20 20:33:03 +0900

    선택정렬 완료

--- 0520/0520/sort.c
+++ 0520/0520/sort.c
@@ -24,7 +24,7 @@ void SwapNumber(int *a, int *b) {
 void BubleSort(int *arr, int len) {
     int i, j;
     for (i = 0; i < len - 1; i++) {
-        for (j = 0; j < len - 1; j++) {
+        for (j = 0; j < len - 1 - j; j++) {
             if (arr[j] > arr[j + 1])
                 SwapNumber(&arr[j], &arr[j + 1]);
         }
@@ -32,9 +32,23 @@ void BubleSort(int *arr, int len) {
         PrintArray(arr, len);
     }
 }
+void SelectSort(int *arr, int len) {
+    //선택정렬 구현
+    int i, j;
+    for (i = 0; i < len - 1; i++) {
+        for (j = i; j < len; j++) {
+            if (arr[j] < arr[i])
+                SwapNumber(&arr[j], &arr[i]);
+        }
+        printf("%d pass : ", i + 1);
+        PrintArray(arr, len);
+    }
+}
 int main(void) {
     int arr1[] = {8,4,6,9,7,1};
-    BubleSort(arr1, 6);
+    int arr2[] = {8,4,6,9,7,1};
+    //BubleSort(arr1, 6);
+    SelectSort(arr2, 6);
     return 0;
 }
 
