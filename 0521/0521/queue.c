ccc53f43c8ac63f75c1afe328ce6739d85991dfa
NamSangKyu <nam2626@gmail.com>
2021-05-21 20:47:59 +0900

    큐 완료

--- 0521/0521/queue.c
+++ 0521/0521/queue.c
@@ -21,10 +21,29 @@ void AddSchedule(Queue *queue) { //enqueue
 }
 void RunSchedule(Queue *queue) {//dequeue
     //큐가 비어있는지 체크
+    if (queue->front == queue->rear) {
+        printf("데이터가 없습니다.\n");
+        return;
+    }
     //데이터를 꺼내서 출력
+    printf("dequeue : %d\n", queue->arr[queue->front]);
     //front 다음 위치로 이동
+    queue->front = (queue->front + 1) % 11;
 }
 
 int main(void) {
+    Queue queue = { {0},0,0 };
+    int no = -1;
+    while (no != 0) {
+        printf("1. Data Enqueue\n");
+        printf("2. Data Dequeue\n");
+        printf("0. Exit\n");
+        printf("input number : ");
+        scanf_s("%d", &no);
+        if (no == 1)
+            AddSchedule(&queue);
+        else if (no == 2)
+            RunSchedule(&queue);
+    }
     return 0;
 }
